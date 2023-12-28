e5f7b3cd03fbc190b54005bab24bfcaccdec4726
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2023-12-28 18:39:25 +0100

    :wastebasket:

--- src/GoogleCloudFunctionsInfo/Assemblies.cs
+++ src/GoogleCloudFunctionsInfo/Assemblies.cs
@@ -6,24 +6,23 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace GoogleCloudFunctionsInfo
+namespace GoogleCloudFunctionsInfo;
+
+public class Assemblies : IHttpFunction
 {
-    public class Assemblies : IHttpFunction
+    private static readonly JsonSerializerOptions Options = new() { WriteIndented = true };
+    private readonly ILogger _logger;
+
+    public Assemblies(ILogger<Assemblies> logger) => _logger = logger;
+
+    public async Task HandleAsync(HttpContext context)
     {
-        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions { WriteIndented = true };
-        private readonly ILogger _logger;
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var result = assemblies.Except(new[] { typeof(Assemblies).Assembly }).OrderBy(x => x.FullName).Select(x => x.ToString()).ToList();
 
-        public Assemblies(ILogger<Assemblies> logger) => _logger = logger;
+        _logger.LogInformation("{Length} assemblies found", assemblies.Length);
 
-        public async Task HandleAsync(HttpContext context)
-        {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            var result = assemblies.Except(new[] { typeof(Assemblies).Assembly }).OrderBy(x => x.FullName).Select(x => x.ToString()).ToList();
-
-            _logger.LogInformation($"{assemblies.Length} assemblies found");
-
-            context.Response.ContentType = "text/json";
-            await JsonSerializer.SerializeAsync(context.Response.Body, result, Options);
-        }
+        context.Response.ContentType = "text/json";
+        await JsonSerializer.SerializeAsync(context.Response.Body, result, Options);
     }
 }
--- src/GoogleCloudFunctionsInfo/Type.cs
+++ src/GoogleCloudFunctionsInfo/Type.cs
@@ -7,94 +7,93 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace GoogleCloudFunctionsInfo
+namespace GoogleCloudFunctionsInfo;
+
+public class Type : IHttpFunction
 {
-    public class Type : IHttpFunction
+    private readonly ILogger _logger;
+
+    public Type(ILogger<Type> logger) => _logger = logger;
+
+    public Task HandleAsync(HttpContext context)
     {
-        private readonly ILogger _logger;
+        var fullName = context.Request.Query.FirstOrDefault(q => string.Compare(q.Key, "FullName", StringComparison.OrdinalIgnoreCase) == 0).Value;
+        var name = context.Request.Query.FirstOrDefault(q => string.Compare(q.Key, "Name", StringComparison.OrdinalIgnoreCase) == 0).Value;
 
-        public Type(ILogger<Type> logger) => _logger = logger;
+        if (!string.IsNullOrEmpty(fullName)) return ByFullName(context, fullName);
 
-        public Task HandleAsync(HttpContext context)
-        {
-            var fullName = context.Request.Query.FirstOrDefault(q => string.Compare(q.Key, "FullName", StringComparison.OrdinalIgnoreCase) == 0).Value;
-            var name = context.Request.Query.FirstOrDefault(q => string.Compare(q.Key, "Name", StringComparison.OrdinalIgnoreCase) == 0).Value;
+        if (!string.IsNullOrEmpty(name)) return ByName(context, name);
 
-            if (!string.IsNullOrEmpty(fullName)) return ByFullName(context, fullName);
-
-            if (!string.IsNullOrEmpty(name)) return ByName(context, name);
-
-            return context.ToResponse(StatusCodes.Status400BadRequest);
-        }
-
-        private Task ByFullName(HttpContext context, string fullName)
-        {
-            var result = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(x => x.GetTypes())
-                .SkipExceptions()
-                .Where(x => x.FullName == fullName)
-                .OrderBy(x => x.Assembly.FullName)
-                .Select(x => x.Assembly.ToString())
-                .ToList();
-
-            _logger.LogInformation($"{fullName} was found in {result.Count} assemblies");
-
-            if (result.Any()) return context.ToOkResponse(result);
-
-            return context.ToResponse(StatusCodes.Status404NotFound);
-        }
-
-        private Task ByName(HttpContext context, string name)
-        {
-            var result = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(x => x.GetTypes())
-                .SkipExceptions()
-                .Where(x => x.Name == name)
-                .OrderBy(x => x.Assembly.FullName)
-                .Select(x => new { Type = x.FullName, Assembly = x.Assembly.ToString() })
-                .ToList();
-
-            _logger.LogInformation($"{name} matched {result.Count} types");
-
-            if (result.Any()) return context.ToOkResponse(result);
-
-            return context.ToResponse(StatusCodes.Status404NotFound);
-        }
+        return context.ToResponse(StatusCodes.Status400BadRequest);
     }
 
-    internal static class Extensions
+    private Task ByFullName(HttpContext context, string fullName)
     {
-        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions { WriteIndented = true };
+        var result = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .SkipExceptions()
+            .Where(x => x.FullName == fullName)
+            .OrderBy(x => x.Assembly.FullName)
+            .Select(x => x.Assembly.ToString())
+            .ToList();
 
-        internal static Task ToOkResponse(this HttpContext context, object result)
-        {
-            context.Response.ContentType = "text/json";
-            return JsonSerializer.SerializeAsync(context.Response.Body, result, Options);
-        }
+        _logger.LogInformation("{FullName} was found in {Count} assemblies", fullName, result.Count);
 
-        internal static Task ToResponse(this HttpContext context, int statusCode)
-        {
-            context.Response.StatusCode = statusCode;
-            return Task.CompletedTask;
-        }
+        if (result.Any()) return context.ToOkResponse(result);
 
-        internal static IEnumerable<T> SkipExceptions<T>(this IEnumerable<T> values)
+        return context.ToResponse(StatusCodes.Status404NotFound);
+    }
+
+    private Task ByName(HttpContext context, string name)
+    {
+        var result = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .SkipExceptions()
+            .Where(x => x.Name == name)
+            .OrderBy(x => x.Assembly.FullName)
+            .Select(x => new { Type = x.FullName, Assembly = x.Assembly.ToString() })
+            .ToList();
+
+        _logger.LogInformation("{Name} matched {Count} types", name, result.Count);
+
+        if (result.Any()) return context.ToOkResponse(result);
+
+        return context.ToResponse(StatusCodes.Status404NotFound);
+    }
+}
+
+internal static class Extensions
+{
+    private static readonly JsonSerializerOptions Options = new() { WriteIndented = true };
+
+    internal static Task ToOkResponse(this HttpContext context, object result)
+    {
+        context.Response.ContentType = "text/json";
+        return JsonSerializer.SerializeAsync(context.Response.Body, result, Options);
+    }
+
+    internal static Task ToResponse(this HttpContext context, int statusCode)
+    {
+        context.Response.StatusCode = statusCode;
+        return Task.CompletedTask;
+    }
+
+    internal static IEnumerable<T> SkipExceptions<T>(this IEnumerable<T> values)
+    {
+        using var enumerator = values.GetEnumerator();
+        var next = true;
+        while (next)
         {
-            using var enumerator = values.GetEnumerator();
-            var next = true;
-            while (next)
+            try
             {
-                try
-                {
-                    next = enumerator.MoveNext();
-                }
-                catch
-                {
-                    continue;
-                }
-
-                if (next) yield return enumerator.Current;
+                next = enumerator.MoveNext();
             }
+            catch
+            {
+                continue;
+            }
+
+            if (next) yield return enumerator.Current;
         }
     }
 }
